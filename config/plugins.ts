3940c37b084063d96a9b19affbfb55e33fff55f5
scriptforprince7 <scriptforprince@gmail.com>
2026-03-20 09:38:33 +0530

    Add jwtSecret to users-permissions plugin config

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,7 @@
-export default () => ({});
+export default ({ env }) => ({
+  'users-permissions': {
+    config: {
+      jwtSecret: env('JWT_SECRET') || 'aB7fK9mP2xQ5tR8wN1zV4cJ6hG3dS7yE9pL2oI5uF0qT8rX3nM6kZ1vC4bA7d',
+    },
+  },
+});
